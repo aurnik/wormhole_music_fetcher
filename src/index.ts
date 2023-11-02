1bf658aefac403a1864703ed809e547214ac6827
aurnik <aurnikislam@gmail.com>
2023-11-02 17:29:13 -0500

    Fix server URL in fetch call

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 console.log(`running your task...`);
-console.log(process.env);
-fetch(`${process.env.SERVER_PRIVATE_DOMAIN}/api/admin/updateAll`, {
+fetch(`https://${process.env.SERVER_PRIVATE_DOMAIN}/api/admin/updateAll`, {
   method: 'POST',
 });
