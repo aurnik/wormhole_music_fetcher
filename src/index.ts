dca0b63f643808858776b30e99ead6bd53a5382d
aurnik <aurnikislam@gmail.com>
2023-11-02 17:33:43 -0500

    Fix API endpoint protocol in fetch call

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 console.log(`running your task...`);
-fetch(`https://${process.env.SERVER_PRIVATE_DOMAIN}/api/admin/updateAll`, {
+fetch(`http://${process.env.SERVER_PRIVATE_DOMAIN}/api/admin/updateAll`, {
   method: 'POST',
 });
